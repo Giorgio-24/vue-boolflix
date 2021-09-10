25c34dc3eb3c6d13fef5af2253b82bf7212e2949
Giorgio Andrei <cipuandrei@libero.it>
2021-09-10 19:21:31 +0200

    header-done

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,43 @@
 <template>
-  <div id="app">ciao</div>
+  <div id="app">
+    <header
+      class="d-flex justify-content-between align-items-center bg-dark px-3"
+    >
+      <h1 class="text-danger text-uppercase">Boolflix</h1>
+      <Searchbar />
+    </header>
+  </div>
 </template>
 
 <script>
-/* import HelloWorld from './components/HelloWorld.vue' */
-
+import Searchbar from "./components/Searchbar.vue";
+import axios from "axios";
 export default {
   name: "App",
-  components: {},
+  components: {
+    Searchbar,
+  },
+  data() {
+    return {
+      selectedMovie: "up",
+      moviesList: {},
+    };
+  },
+  created() {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=a49fd2ad1915c16f5b21a815b7e90362&language=it-IT&query=${this.selectedMovie}`
+      )
+      .then(function (res) {
+        this.moviesList = res;
+      })
+      .catch(function (warning) {
+        console.log(warning);
+      });
+  },
 };
 </script>
 
 <style lang="scss">
+@import "./assets/scss/_style.scss";
 </style>
---
+++ src/components/Searchbar.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="input-group mb-3">
+      <input
+        type="text"
+        class="form-control border-0"
+        placeholder="Search..."
+        aria-label="Recipient's username"
+        aria-describedby="basic-addon2"
+      />
+      <span
+        class="input-group-text border-0 bg-danger text-white"
+        id="basic-addon2"
+        >Search</span
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Searchbar",
+  props: ["moviesList"],
+};
+</script>
+
+<style style="scss" scoped>
+</style>
